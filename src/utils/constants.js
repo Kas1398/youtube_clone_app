9c86fac27e7eb223183709f6790f390b257459d5
kassahun <kassuhan@gmail.com>
2023-03-29 11:53:16 +0300

    added files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,7 +16,8 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
     { name: 'New', icon: <HomeIcon />, },
-    { name: 'JS Mastery', icon: <CodeIcon />, },
+    // { name: 'JS Mastery', icon: <CodeIcon />, },
+    { name: 'ebstv worldwide', icon: <LiveTvIcon />, },
     { name: 'Coding', icon: <CodeIcon />, },
     { name: 'ReactJS', icon: <CodeIcon />, },
     { name: 'NextJS', icon: <CodeIcon />, },
@@ -34,9 +35,9 @@ export const categories = [
     { name: 'Crypto', icon: <DeveloperModeIcon />, },
 ];
 
-export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
-export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
-export const demoVideoUrl = '/video/GDa8kZLNhJ4';
-export const demoChannelTitle = 'JavaScript Mastery';
-export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoThumbnailUrl = 'https://en.wikipedia.org/wiki/EBS_TV_(Ethiopia)#/media/File:EBS_TV_(Ethiopia).png'//'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = 'c/ebstvWorldwide' //'/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = 'watch?v=sqDYNH9rnNY&ab_channel=ebstvworldwide' //'/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'ebstv worldwide'//'JavaScript Mastery';
+export const demoVideoTitle = 'Besintu sitcom' //'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
 export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
